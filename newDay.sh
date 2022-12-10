00976f76e2956bada6c86cd8fb1127ec0b8d309d
Niko Sward <nicholascsward@gmail.com>
2022-12-10 16:40:11 -0600

    new day script updated

--- newDay.sh
+++ newDay.sh
@@ -4,14 +4,6 @@ read -p 'Day: ' dayNum
 
 mkdir $dayNum
 cd $dayNum
-echo "/*
-
-*/
-const {getInput, getInputArray, print} = require('../../utils');
-const input = getInput(__dirname, '/test1.txt');
-const start = Date.now();
-
-console.log('Time', Date.now() - start, 'ms');
-" >> 1.js
+cp ../../newDayTemplate.js 1.js
 touch input.txt
 touch test1.txt
---
+++ newDayTemplate/1.js
@@ -0,0 +1,14 @@
+/*
+
+*/
+const { getInputArray } = require("../../utils");
+const input = getInputArray(__dirname, "/test1.txt");
+const start = Date.now();
+
+function main() {
+
+}
+
+console.log(main());
+
+console.log("Time", Date.now() - start, "ms");
