22bb98f92bbe943910e5687b313f1d9a9e285eba
Niko Sward <nicholascsward@gmail.com>
2024-12-02 00:04:10 -0600

    days 1 & 2 of 2024

---
+++ 2024/1/2.js
@@ -0,0 +1,55 @@
+/**
+ * --- Part Two ---
+Your analysis only confirmed what everyone feared: the two lists of location IDs are indeed very different.
+
+Or are they?
+
+The Historians can't agree on which group made the mistakes or how to read most of the Chief's handwriting, but in the commotion you notice an interesting detail: a lot of location IDs appear in both lists! Maybe the other numbers aren't location IDs at all but rather misinterpreted handwriting.
+
+This time, you'll need to figure out exactly how often each number from the left list appears in the right list. Calculate a total similarity score by adding up each number in the left list after multiplying it by the number of times that number appears in the right list.
+
+Here are the same example lists again:
+
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+For these example lists, here is the process of finding the similarity score:
+
+The first number in the left list is 3. It appears in the right list three times, so the similarity score increases by 3 * 3 = 9.
+The second number in the left list is 4. It appears in the right list once, so the similarity score increases by 4 * 1 = 4.
+The third number in the left list is 2. It does not appear in the right list, so the similarity score does not increase (2 * 0 = 0).
+The fourth number, 1, also does not appear in the right list.
+The fifth number, 3, appears in the right list three times; the similarity score increases by 9.
+The last number, 3, appears in the right list three times; the similarity score again increases by 9.
+So, for these example lists, the similarity score at the end of this process is 31 (9 + 4 + 0 + 0 + 9 + 9).
+
+Once again consider your left and right lists. What is their similarity score?
+ */
+
+const { getInputArray } = require("../../utils");
+const start = Date.now();
+
+function main() {
+  const input = getInputArray(__dirname, "/input.txt");
+  const left = [];
+  const right = [];
+  for (const line of input) {
+    const [l, r] = line.split("   ");
+    left.push(Number(l));
+    right.push(Number(r));
+  }
+  let answer = 0;
+  for (let i = 0; i < left.length; i++) {
+    let cur = left[i];
+    let multiplier = right.filter((x) => x == cur).length;
+    answer += cur * multiplier;
+  }
+  return answer;
+}
+
+console.log(main());
+
+console.log("Time", Date.now() - start, "ms");
